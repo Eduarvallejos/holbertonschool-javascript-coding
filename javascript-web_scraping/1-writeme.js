205f74487012d0015080d4a4e4306e3a2428655b
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-04-07 14:50:12 -0500

    writing to a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filepath = process.argv[2];
+const string = process.argv[3];
+
+if (!filepath) {
+  console.log('Error: invalid route');
+} else {
+  fs.writeFile(filepath, string, 'utf-8', (error) => {
+    if (error) {
+      console.log('An error occurred while writing to the file', error);
+    }
+  });
+}
