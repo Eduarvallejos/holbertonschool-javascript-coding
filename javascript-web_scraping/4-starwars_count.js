74762a491d3533c5c8cfb1ca69a93ad87f1524d3
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-04-07 21:15:22 -0500

    Star wars Wedge Antilles

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // This script prints the number of movies where the character 'Wedge Antilles' is present.
 
-
 const request = require('request');
 
 const apiUrl = process.argv[2];
@@ -9,7 +8,6 @@ const apiUrl = process.argv[2];
 if (!apiUrl) {
   console.log('Error: Please provide the API URL of the Star Wars API.');
 } else {
-  const characterId = '18';
   request.get(apiUrl, (error, response, body) => {
     if (error) {
       console.log('Error:', error);
@@ -17,14 +15,13 @@ if (!apiUrl) {
       console.log('Error:', response.statusCode);
     } else {
       const films = JSON.parse(body).results;
-      let count = 0;
+      let count = [];
       films.forEach(film => {
-        if (film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)) {
+        if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
           count++;
         }
       });
       console.log(count);
     }
   });
-};
-
+}
