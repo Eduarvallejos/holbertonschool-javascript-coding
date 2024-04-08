0e30edbeb786378838ded1b65435a4555853b75d
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-04-07 21:33:31 -0500

    Loripsum

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,30 +1,29 @@
 #!/usr/bin/node
 // This script gets the content of a web page and stores it in a file.
 
-
 const request = require('request');
 const fs = require('fs');
-
 const url = process.argv[2];
 const filePath = process.argv[3];
 
 if (!url || !filePath) {
-  console.log('Error: Provide the URL and path of the destination file.');
-} else {
-  request.get(url, (error, response, body) => {
-    if (error) {
-      console.log('Error:', error);
-    } else if (response.statusCode !== 200) {
-      console.log('Error:', response.statusCode);
-    } else {
-      fs.writeFile(filePath, body, 'utf-8', (err) => {
-        if (err) {
-          console.log('Error writing to file:', err);
-        } else {
-          console.log('Webpage content saved to', filePath);
-        }
-      });
-    }
-  });
-};
+  console.log('Provide the URL and path of the destination file');
+  process.exit(1);
+}
 
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  if (response.statusCode === 200) {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  } else {
+    console.log(`Error: ${response.statusCode}`);
+  }
+});
