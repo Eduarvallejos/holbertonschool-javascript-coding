35b2140f41da9c256e8cbf0e8787dde32f4c9fc2
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-05-05 16:54:47 -0500

    Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,12 +3,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk){
-    process.stdout.write(`Your name is: ${chunk}`);
+  const text = process.stdin.read();
+  if (text){
+    process.stdout.write(`Your name is: ${text}`);
+    process.stdout.write('This important software is now closing\n');
   }
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+
